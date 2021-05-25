bb839fedb67f0081224cea32424d05676d8d0124
kksumm123 <kksumm123@gmail.com>
2021-05-25 21:37:49 +0900

    왜 vs로 푸쉬 안되냐 ㅠㅠ

---
+++ Assets/Monster.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    // 앞으로 움직여라.
+    // 벽을 만나면 뒤로 방향전환
+
+    new Rigidbody2D rigidbody2D;
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    public float speed = 0.1f;
+    void FixedUpdate()
+    {
+        var pos = rigidbody2D.position;
+        pos.x += speed * transform.forward.z;
+        rigidbody2D.position = pos;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        transform.rotation =
+            new Quaternion(0, (transform.rotation.y == 0 ? 180 : 0), 0, 0);
+    }
+}
