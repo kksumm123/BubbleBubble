d7a9b36085798791330d62ddff18687f5ca76683
kksumm123 <kksumm123@gmail.com>
2021-05-27 21:43:50 +0900

    씬 전환 완료

--- Assets/Monster.cs
+++ Assets/Monster.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Monster : MonoBehaviour
 {
+    public List<Monster> Items = new List<Monster>();
+    private void OnDestroy()
+    {
+        Items.Remove(this);
+        if (Items.Count == 0)
+            SceneManager.LoadScene("Stage2");
+    }
     // 앞으로 움직여라.
     // 벽을 만나면 뒤로 방향전환
 
     new Rigidbody2D rigidbody2D;
     private void Awake()
     {
+        Items.Add(this);
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
     public float speed = 0.1f;
